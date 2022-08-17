899342b1d17e3fcd8fd57672fa525ef95fb5d8bc
kozan <kozan@LAPTOP-83OBPT96.localdomain>
2022-08-18 00:46:53 +0300

    saved and loaded any model to json format

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 ### Install Cuda toolkit and Cudnn for tensorflow in WSL2 Ubuntu22.04
 
-activate virtualenv
+Firstly you should check your GPU and its Compute Capability. if you want to use 
+CUDA toolkit support for Tensorflow your GPU has at least 3.5 Compute Capability.
+I have NVIDIA GTX 1660 Ti and it has 7.5 Com. Capa.
+
+
+Activate Virtualenv Command:
 - source /home/kozan/.virtualenvs/TF_Deep_Learing_book/bin/activate
--- app1/main.py
+++ app1/main.py
@@ -1,41 +1,43 @@
-import tensorflow as tf
 from keras.models import Sequential
 from keras.layers import Dense
-from sklearn.model_selection import train_test_split
+from keras.models import model_from_json
 import numpy
 
-########################################
-tf.compat.v1.disable_v2_behavior()  # disable tf 2 features
-tf.compat.v1.disable_eager_execution()
-tf1 = tf.compat.v1  # tf v1 reached functions
-a = tf1.placeholder(tf.float32)
-b = tf1.placeholder(tf.float32)
-add = tf1.add(a, b)
-sess = tf1.Session()
-binding = {a: 1.5, b: 2.5}
-c = sess.run(add, feed_dict=binding)
-########################################
 
-seed = 7
-numpy.random.seed(seed)
-
+numpy.random.seed(7)
 dataset = numpy.loadtxt("pima-indians-diabetes.csv", delimiter=",")
-# split into input and output variables
-X = dataset[:, 0:8]
-Y = dataset[:, 8]   # label
 
-X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.33, random_state=seed)
+X = dataset[:,0:8]
+Y = dataset[:,8]
 
 model = Sequential()
-model.add(Dense(12, input_dim=8, activation='relu'))
-model.add(Dense(8, activation='relu'))
-model.add(Dense(1, activation='sigmoid'))  # between o and 1
+model.add(Dense(12, input_dim=8, kernel_initializer='uniform', activation='relu'))
+model.add(Dense(8, kernel_initializer='uniform', activation='relu'))
+model.add(Dense(1, kernel_initializer='uniform', activation='sigmoid'))
 
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=150, batch_size=10)  # CPU and GPU works in here
+model.fit(X, Y, epochs=150, batch_size=10, verbose=0)
 
-scores = model.evaluate(X, Y)
+scores = model.evaluate(X, Y, verbose=0)
+print("%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
 
-if __name__ == '__main__':
-    print('\n%s: %.2f%%' % (model.metrics_names[1], scores[1]*100))
+model_json = model.to_json()
+
+with open("model.json", "w") as json_file:
+    json_file.write(model_json)
+# serialize weights to HDF5
+model.save_weights("model.h5")
+print("Saved model to disk")
+
+json_file = open('model.json', 'r')
+loaded_model_json = json_file.read()
+
+json_file.close()
+loaded_model = model_from_json(loaded_model_json)
+loaded_model.load_weights("model.h5")
+
+print("Loaded model from disk")
+loaded_model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
+score = loaded_model.evaluate(X, Y, verbose=0)
+print("%s: %.2f%%" % (loaded_model.metrics_names[1], score[1]*100))

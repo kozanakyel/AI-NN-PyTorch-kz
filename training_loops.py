7d7916a95555b83057e21a3de6e0c0bd41f042bc
kozanakyel <kozanakyel@gmail.com>
2024-01-11 20:36:52 +0100

    trainign loops reviewing in pytorch with high order functions

---
+++ training_loops.py
@@ -0,0 +1,99 @@
+import numpy as np
+from sklearn.linear_model import LinearRegression
+import torch
+import torch.optim as optim
+import torch.nn as nn
+from torch.utils.data import Dataset, TensorDataset, DataLoader
+from torch.utils.data.dataset import random_split
+from torch.utils.tensorboard import SummaryWriter
+
+import matplotlib.pyplot as plt
+from data_generation.simple_linear_regression import *
+
+
+# higher order functions
+def square(x):
+    return x ** 2
+
+
+def cube(x):
+    return x ** 3
+
+
+def fourth_power(x):
+    return x ** 4
+
+
+def generic_exponentiation(x, exponent):
+    return x ** exponent
+
+
+# def skeleton_exponentiation(x):
+#     return x ** exponent
+def exponentiation_builder(exponent):
+    def skeleton_exponentiation(x):
+        return x ** exponent
+
+    return skeleton_exponentiation
+
+
+def make_train_step_fn(model, loss_fn, optimizer):
+    # Builds function that performs a step in the train loop
+    def perform_train_step_fn(x, y):
+        # Sets model to TRAIN mode
+        model.train()
+
+        # Step 1 - Computes our model's predicted output - forward pass
+        yhat = model(x)
+        # Step 2 - Computes the loss
+        loss = loss_fn(yhat, y)
+        # Step 3 - Computes gradients for both "a" and "b" parameters
+        loss.backward()
+        # Step 4 - Updates parameters using gradients and the learning rate
+        optimizer.step()
+        optimizer.zero_grad()
+
+        # Returns the loss
+        return loss.item()
+
+    # Returns the function that will be called inside the train loop
+    return perform_train_step_fn
+
+
+# data preparation
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+# Our data was in Numpy arrays, but we need to transform them
+# into PyTorch's Tensors and then we send them to the
+# chosen device
+x_train_tensor = torch.as_tensor(x_train).float().to(device)
+y_train_tensor = torch.as_tensor(y_train).float().to(device)
+
+# Sets learning rate - this is "eta" ~ the "n" like Greek letter
+lr = 0.1
+
+torch.manual_seed(42)
+# Now we can create a model and send it at once to the device
+model = nn.Sequential(nn.Linear(1, 1)).to(device)
+
+# Defines a SGD optimizer to update the parameters (now retrieved directly from the model)
+optimizer = optim.SGD(model.parameters(), lr=lr)
+
+# Defines a MSE loss function
+loss_fn = nn.MSELoss(reduction='mean')
+
+# Creates the train_step function for our model, loss function and optimizer
+train_step_fn = make_train_step_fn(model, loss_fn, optimizer)
+
+print(f'train step: {train_step_fn}')
+
+n_epochs = 1000
+
+losses = []
+
+for epoch in range(n_epochs):
+    loss = train_step_fn(x_train_tensor, y_train_tensor)
+    losses.append(loss)
+
+print(model.state_dict())

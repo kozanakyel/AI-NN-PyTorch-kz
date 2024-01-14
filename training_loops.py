8b6e785951734b6a5a222bed9a2715056836065e
root <kozanakyel@gmail.com>
2024-01-14 08:38:17 +0000

    edited for training loops study and pytorch

--- training_loops.py
+++ training_loops.py
@@ -86,7 +86,7 @@ loss_fn = nn.MSELoss(reduction='mean')
 # Creates the train_step function for our model, loss function and optimizer
 train_step_fn = make_train_step_fn(model, loss_fn, optimizer)
 
-print(f'train step: {train_step_fn}')
+# print(f'train step: {train_step_fn}')
 
 n_epochs = 10
 
@@ -96,7 +96,7 @@ for epoch in range(n_epochs):
     loss = train_step_fn(x_train_tensor, y_train_tensor)
     losses.append(loss)
 
-print(model.state_dict())
+# print(model.state_dict())
 
 
 class CustomDataset(Dataset):
@@ -115,10 +115,11 @@ x_train_tensor = torch.as_tensor(x_train).float()
 y_train_tensor = torch.as_tensor(y_train).float()
 
 train_data = CustomDataset(x_train_tensor, y_train_tensor)
-print(train_data[0])
+
+# print(train_data[0])
 
 train_data = TensorDataset(x_train_tensor, y_train_tensor)
-print(train_data[0])
+# print(train_data[0])
 
 train_loader = DataLoader(
     dataset=train_data,
@@ -126,7 +127,7 @@ train_loader = DataLoader(
     shuffle=True,
 )
 
-print(next(iter(train_loader)))  # one mini-batch with 16 data
+# print(next(iter(train_loader)))  # one mini-batch with 16 data
 
 n_epochs = 100
 losses = []
@@ -143,7 +144,7 @@ for epoch in range(n_epochs):
     loss = np.mean(mini_batch_losses)
     losses.append(loss)
 
-print(model.state_dict())
+# print(model.state_dict())
 
 
 def mini_batch(device, data_loader, step_fn):
@@ -165,6 +166,110 @@ for epoch in range(n_epochs):
     losses.append(loss)
 
 print(model.state_dict())
+
+
+torch.manual_seed(13)
+
+# Builds tensors from numpy arrays BEFORE split
+x_tensor = torch.as_tensor(x).float()
+y_tensor = torch.as_tensor(y).float()
+
+# Builds dataset containing ALL data points
+dataset = TensorDataset(x_tensor, y_tensor)
+
+# Performs the split
+ratio = .8
+n_total = len(dataset)
+n_train = int(n_total * ratio)
+n_val = n_total - n_train
+
+train_data, val_data = random_split(dataset, [n_train, n_val])
+
+# Builds a loader of each set
+train_loader = DataLoader(dataset=train_data, batch_size=16, shuffle=True)
+val_loader = DataLoader(dataset=val_data, batch_size=16)
+
+
+def make_val_step_fn(model, loss_fn):
+    def perform_val_step_fn(x,y):
+      model.eval()
+
+      yhat = model(x)
+
+      loss = loss_fn(yhat, y)
+
+      return loss.item()
+
+    return perform_val_step_fn
+
+
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+# Sets learning rate - this is "eta" ~ the "n" like Greek letter
+lr = 0.1
+
+torch.manual_seed(42)
+# Now we can create a model and send it at once to the device
+model = nn.Sequential(nn.Linear(1, 1)).to(device)
+
+# Defines a SGD optimizer to update the parameters (now retrieved directly from the model)
+optimizer = optim.SGD(model.parameters(), lr=lr)
+
+# Defines a MSE loss function
+loss_fn = nn.MSELoss(reduction='mean')
+
+# Creates the train_step function for our model, loss function and optimizer
+train_step_fn = make_train_step_fn(model, loss_fn, optimizer)
+val_step_fn = make_val_step_fn(model, loss_fn)
+
+losses = []
+val_losses = []
+for epoch in range(n_epochs):
+    loss = mini_batch(device, train_loader, train_step_fn)
+    losses.append(loss)
+
+    with torch.no_grad():
+        val_loss = mini_batch(device, val_loader, val_step_fn)
+        val_losses.append(val_loss)
+
+print(model.state_dict())
+
+checkpoint = {'epoch': n_epochs,
+              'model_state_dict': model.state_dict(),
+              'optimizer_state_dict': optimizer.state_dict(),
+              'loss': losses,
+              'val_loss': val_losses}
+torch.save(checkpoint, 'model_checkpoint.pth')
+
+
+# model load and resuming for trainign interesting...
+checkpoint = torch.load('model_checkpoint.pth')
+model.load_state_dict(checkpoint['model_state_dict'])
+optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+saved_epoch = checkpoint['epoch']
+saved_losses = checkpoint['loss']
+saved_val_losses = checkpoint['val_loss']
+model.train()
+
+
+print(model.state_dict())
+
+checkpoint = torch.load('model_checkpoint.pth')
+model.load_state_dict(checkpoint['model_state_dict'])
+print(model.state_dict())
+
+
+new_inputs = torch.tensor([[.20], [.34], [.57]])
+model.eval() # always use EVAL for fully trained models! ①
+model(new_inputs.to(device))
+
+
+
+
+
+
+
 
 
 
